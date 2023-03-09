fc4435aa9cafa0a38a3404b49f9a498385459b5b
Andrea Mangrella <mangrella.andrea@gmail.com>
2023-03-09 21:45:03 +0100

    Added basic structure to teamBox and fixed overall structure

---
+++ components/TeamBox.js
@@ -0,0 +1,12 @@
+import Swal from "sweetalert2"
+
+export default function TeamBox({handler,classroomData}){
+  return (
+    <div>
+      <button
+       className=""
+       onClick={handler}
+      />
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import styles from '@/styles/Home.module.css'
 import { useState,useEffect, useRef, use } from 'react'
-import Head from 'next/head'
-// import TeamBox from "../components/TeamBox"
+import Swal from "sweetalert2"
+import TeamBox from "../components/TeamBox"
 import Layout from "../components/Layout"
 
 import { useUser } from "@auth0/nextjs-auth0/client"
@@ -9,12 +8,15 @@ import {getSession} from "@auth0/nextjs-auth0"
 import axios from "axios"
 
 export const getServerSideProps = ({res,req}) => {
+  url = "https://" + process.env.BACKEND_URI
   const session = getSession(req, res)
   if (!session) return { props: {} }
-  return { props: { token: session.accessToken }}
+  return { props: { token: session.accessToken, url: url}}
 }
 
-export default function Home({accessToken}) {
+
+
+export default function Home({accessToken,url}) {
   const { user, isLoading } = useUser()
   const [classroom_tiles,setClassroom_tiles] = useState([])
   useEffect(()=>{
@@ -23,9 +25,31 @@ export default function Home({accessToken}) {
     }
   },[user])
 
+  // Handler for the TeamBoxes that uses almost everything 
+  const addBoxHandler = async ()=>{
+    const { value: id } = await Swal.fire({
+      title: "Create a New Class",
+      input: "text",
+      inputPlaceholder: "Name the class",
+      showCancelButton: true,
+      closeOnCancel: true,
+      confirmButtonColor: "#FFB900",
+      cancelButtonColor: "#374151",
+      confirmButtonText: 'Create',
+    })
+    const newClass  = await axios({
+      method: "get",
+      url: url + "/user/addBox/"+id,
+      headers: { Authorization: "Bearer " + token }
+    })
+    setClassroom_tiles([...classroom_tiles,newClass]) 
+    //if (id) router.push({ pathname: "/form", query: { draftID: id } })
+  }
+  
   return (
     <>
-      <Layout>      
+      <Layout user={user} loading={isLoading}>      
+        Testo di prova Children 
       </Layout>      
     </>
   )
