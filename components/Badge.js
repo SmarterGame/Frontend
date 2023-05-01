45f714bb0a6d44d4a91911d0726bd59bbb3f88ff
Stefano <sacchet13@gmail.com>
2023-05-01 14:17:30 +0200

    Optimizing images loading

--- components/Badge.js
+++ components/Badge.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import ContentLoader from "react-content-loader";
 
 export default function Badge({ token, url, badge }) {
     const [badgeData, setBadgeData] = useState({});
     const [badgeImg, setBadgeImg] = useState("");
+    const [imageLoaded, setImageLoaded] = useState(false);
 
     useEffect(() => {
         const getBadge = async () => {
@@ -37,15 +40,51 @@ export default function Badge({ token, url, badge }) {
         getBadgeImg();
     }, []);
 
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setImageLoaded(true);
+    //     }, 1000); // imposta un ritardo di 1 secondo
+    //     return () => clearTimeout(timer);
+    // }, []);
+
+    const handleImageLoaded = () => {
+        const timer = setTimeout(() => {
+            setImageLoaded(true);
+        }, 1000); // imposta un ritardo di 1 secondo
+        return () => clearTimeout(timer);
+    };
+
     return (
         <>
             <div className="flex felx-row items-center justify-center gap-x-6 w-full mt-6">
-                {/* <div className="w-20 h-20 bg-gray-700 rounded-full"></div> */}
+                {imageLoaded ? (
+                    //Visualize image
+                    <Image
+                        src={badgeImg}
+                        alt="badge image"
+                        width={130}
+                        height={130}
+                    />
+                ) : (
+                    //Visualize skeleton
+                    <ContentLoader
+                        speed={2}
+                        width={130}
+                        height={100}
+                        viewBox="0 0 130 100"
+                        backgroundColor="#f3f3f3"
+                        foregroundColor="#ffffff"
+                    >
+                        <circle cx={50} cy={50} r="50" />
+                    </ContentLoader>
+                )}
                 <Image
                     src={badgeImg}
                     alt="badge image"
                     width={130}
-                    height={100}
+                    height={130}
+                    style={{ display: "none" }}
+                    onLoad={() => setImageLoaded(true)}
                 />
                 <div className="flex flex-col bg-slate-100 w-[65%] h-[60%] rounded-xl">
                     <h1 className="mx-auto text-lg text-orangeBtn mt-2">
