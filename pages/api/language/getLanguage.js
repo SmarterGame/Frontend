bbd21bfdc7e40a2305c9d4a057e34fc7e614dc86
Stefano <sacchet13@gmail.com>
2023-06-23 20:26:51 +0200

    Fix

--- pages/api/language/getLanguage.js
+++ pages/api/language/getLanguage.js
@@ -1,13 +1,7 @@
 import fs from "fs";
 
 export default async function getLanguage(req, res) {
-    //get the path of the file
-    // const path = "./data/language.txt";
-    //tieni solo i primi 5 elementi dell'array
-    const pathArray = __dirname.split("\\").slice(0, 6);
-    //unisci gli elementi dell'array in un path
-    const path = pathArray.join("\\") + "/public/data/language.txt";
-    // console.log(path);
+    const path = "./public/data/language.txt";
 
     try {
         const data = fs.readFileSync(path, "utf8");
