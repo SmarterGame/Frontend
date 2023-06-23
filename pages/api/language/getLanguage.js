1817670db98778a3d51e9b5706e5e4496f5361c5
Stefano <sacchet13@gmail.com>
2023-06-23 20:30:30 +0200

    Fail

--- pages/api/language/getLanguage.js
+++ pages/api/language/getLanguage.js
@@ -1,8 +1,7 @@
 import fs from "fs";
 
 export default async function getLanguage(req, res) {
-    // const path = `${process.env.VERCEL_URL}/data/language.txt`;
-    const path = "https://frontend-taupe-kappa-35.vercel.app/data/language.txt"
+    const path = "./public/data/language.txt";
 
     try {
         const data = fs.readFileSync(path, "utf8");
