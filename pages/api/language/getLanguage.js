d837358048e44759b76c39eca9ba38e8731016f3
Stefano <sacchet13@gmail.com>
2023-06-23 20:21:47 +0200

    Try

--- pages/api/language/getLanguage.js
+++ pages/api/language/getLanguage.js
@@ -2,9 +2,12 @@ import fs from "fs";
 
 export default async function getLanguage(req, res) {
     //get the path of the file
-    const path = "./data/language.txt";
-    // const vercelUrl = process.env.VERCEL_URL;
-    // console.log(vercelUrl);
+    // const path = "./data/language.txt";
+    //tieni solo i primi 5 elementi dell'array
+    const pathArray = __dirname.split("\\").slice(0, 6);
+    //unisci gli elementi dell'array in un path
+    const path = pathArray.join("\\") + "/public/data/language.txt";
+    // console.log(path);
 
     try {
         const data = fs.readFileSync(path, "utf8");
