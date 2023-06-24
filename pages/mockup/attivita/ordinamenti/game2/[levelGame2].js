3830e4108b814d36936703b5faf05968d571c07c
Stefano <sacchet13@gmail.com>
2023-06-24 14:22:18 +0200

    Minor changes

--- pages/mockup/attivita/ordinamenti/game2/[levelGame2].js
+++ pages/mockup/attivita/ordinamenti/game2/[levelGame2].js
@@ -325,9 +325,9 @@ export default function Game({
 
     return (
         <>
-            <button onClick={gameFinished} className="bg-red-500">
+            {/* <button onClick={gameFinished} className="bg-red-500">
                 test API
-            </button>
+            </button> */}
 
             <LayoutGames
                 classRoom={classRoom}
