93d6898e0a43a7c5bf89558a71694e7b3666dab9
Davide Frageri <davide.frageri@studenti.unitn.it>
2024-05-08 19:12:30 +0200

    fix not passing exec

--- pages/mockup/attivita/[gameId].js
+++ pages/mockup/attivita/[gameId].js
@@ -269,7 +269,7 @@ export default function Game({
     const {events, sendAction} = useSmarter({smarterIds: selectedSmarters?.map(smarter => smarter.name) ?? []});
     const [error, setError] = useState(false);
     const [currentExe, setCurrentExe] = useState(0);
-    const [lvlDataCorrect, setLvlDataCorrect] = useState(game?.levels[+level-1]?.exercises?.[currentExe]?.endSeq ?? []);
+    const [lvlDataCorrect, setLvlDataCorrect] = useState(game?.levels[+level-1]?.exercises?.[currentExe]?.endSeq?.map(item => item === "_" ? "" : item) ?? []);
     const [lvlData, setLvlData] = useState(game?.levels[+level-1]?.exercises?.[currentExe]?.startSeq?.map(item => item === "_" ? "" : item) ?? []); //Used to check the correct solution
     const [inputValues, setInputValues] = useState(new Array(selectedSmarters?.length*5).map(() => "")); //Used to store the input values
     const [isCorrect, setIsCorrect] = useState([
